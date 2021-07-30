236f5e8f864012e0d0e45706566249f655cc43e4
Reoup <71483084+Reoup@users.noreply.github.com>
2021-07-30 17:59:56 +0900

    Update video_list_item.js

--- src/componenets/video_list_item.js
+++ src/componenets/video_list_item.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoListItem = ({video, onVideoSelect}) => {
     const imageUrl = video.snippet.thumbnails.default.url; // video의 snippet의 thumbnails의 default의 url
     return (
-        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+        <li onClick={() => onVideoSelect(video)} className="list-group-item"> {/*이 프로퍼티는 클릭할 때마다 전달한 함수를 호출*/ }
             <div className="video-list media">
                 <div className="media-left">
                     <img className="media-object" src={imageUrl}></img>
